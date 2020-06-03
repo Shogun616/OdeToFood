f2a77c0fc35f7333683f822146e4c7d5f7f303c2
Philip Mattsson <maini.ac@hotmail.com>
2020-06-03 19:18:44 +0200

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using OdeToFood.Core;
+using System.Collections.Generic;
+
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Freddy Fazbear's Pizza", Location = "Los Angelos", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "New India", Location = "New Jork", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 3, Name = "Tacobar", Location = "Austin", Cuisine = CuisineType.Mexican }
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
